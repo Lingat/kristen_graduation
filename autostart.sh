4a22e2da8718f29c3a60b175695534189fc4c203
Lingat <lingat917@gmail.com>
2026-03-25 01:22:49 +1100

    fixed start script

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,18 @@
-# auto start script for raspberry pi
-cd kristen_graduation
-nvm use 20
+#!/bin/bash
+# 1. Load NVM environment (Crucial for autostart)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# 2. Move to project folder
+cd ~/kristen_graduation || exit
+
+# 3. Update and Start Node
 git pull
+nvm use 20
 node server.js &
+
+# 4. Wait a few seconds for the server to actually breathe
+sleep 5
+
+# 5. Launch Chrome
 chromium-browser http://localhost:3000 --start-fullscreen
